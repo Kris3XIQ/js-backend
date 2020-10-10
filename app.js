4453138925e8ce7841e7fbb2b3258c64da1b5b0c
Kristoffer Markow <Kristoffer.markow@gmail.com>
2020-10-11 00:21:32 +0100

    Made changes to app.js, mainly adding socket ios for chat integration

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ require('dotenv').config();
 
 const port = 3080;
 const express = require("express");
+const app = express();
+const server = app.listen(port, logStartUpDetailsToConsole);
+const io = require("socket.io")(server, { origins: "*:*" });
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 // const morgan = require("morgan");
 // const SECRET_KEY = require("./config/config");
+
 const routeIndex = require("./routes/index");
 const routeReports = require("./routes/reports");
 const routeAccount = require("./routes/account");
 const middleware = require("./middleware/index");
-const app = express();
 
 app.use(cors());
 
@@ -25,8 +28,32 @@ app.use("/", routeIndex);
 app.use("/reports", routeReports);
 app.use("/account", routeAccount);
 
+// if (process.env.NODE_ENV !== "test") {
+//     app.listen(port, logStartUpDetailsToConsole);
+// }
 if (process.env.NODE_ENV !== "test") {
-    app.listen(port, logStartUpDetailsToConsole);
+    io.on("connect", function(socket) {
+        const { id } = socket.client;
+        const time = new Date;
+        const now = getTimeStamp(time);
+        socket.on("chat message", ({ nick, msg }) => {
+            io.emit("chat message", { now, nick, msg });
+        });
+    });
+
+    function getTimeStamp(date) {
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        let prettyNow = hours + ':' + minutes + ' ' + ampm;
+        return prettyNow;
+    }
+} else {
+    const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    module.exports = server;
 }
 
 /**
@@ -58,6 +85,6 @@ function logStartUpDetailsToConsole() {
     console.info(routes);
 }
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-module.exports = server;
+// module.exports = server;
